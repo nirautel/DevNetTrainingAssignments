1474a2d7f07bc5354af6c966a022ae96267df158
Nitesh R <nirautel@cisco.com>
2020-06-02 17:15:35 +0530

    Changes to Errors in Code

--- Day2/API_Device_Parsing.py
+++ Day2/API_Device_Parsing.py
@@ -1,24 +1,27 @@
 import json
 import requests
+import base64
 
 #Pulling the Token
-def Pull_Token():
+def Pull_token():
     URL= 'https://sandboxdnac2.cisco.com/api/system/v1/auth/token'
     data={}
+    #EncodedCreds = base64.b64encode(bytes(LoginCreds['Username']+':'+LoginCreds['Password'],"utf8")).decode("ascii")
     headers = {
-        'Authorisation': 'Basic ZG5hY2RldjpEM3Y5M1RAd0sh'
+        'Authorization': 'Basic ZG5hY2RldjpEM3Y5M1RAd0sh'
     }
     response = requests.request("POST", URL, headers=headers, data=data)
+    json_data = json.loads(response.text.encode('utf8'))
 
-    data_json = json.loads(response.text.encode('utf8'))
-    return data_json["Token"]
+    return json_data["Token"]
 
 
-def Show_Data(Token):
+def Show_Data(token):
     URL= 'https://sandboxdnac2.cisco.com/dna/intent/api/v1/network-device'
     data={}
+
     headers = {
-        'x-auth-token': Token,
+        'x-auth-token': token
     }
     response = requests.request("GET", URL, headers=headers, data=data)
     json_data = json.loads(response.text.encode('utf8'))
@@ -35,6 +38,6 @@ def Show_Data(Token):
 
 
 if __name__ == '__main__':
-    Token = Pull_Token()
-    Show_Data(Token)
+    token = Pull_token()
+    Show_Data(token)
 
