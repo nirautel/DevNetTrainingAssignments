0589c5d3e661d852be7f4d0ca6c43b11dd6248b0
Nitesh R <nirautel@cisco.com>
2020-06-02 11:42:15 +0530

    changed snapshot and codes

--- data/Script_devices_Parsing.py
+++ data/Script_devices_Parsing.py
@@ -1,17 +1,18 @@
 import json
 
-#opening the json file
+#Opening the json file
 file_json = open('dnac_devices.json')
 json_data = json.load(file_json)
 
-#parsing the data for the required attributes
+#Parsing the data for the required attributes
 for line in json_data['response']:
-    print('Item ID :   '+line["id"])
-    print('Item TYPE :   '+line["type"])
-    print('Item FAMILY :   '+line["family"])
-    print('Item SOFTWARE :   '+line["softwareType"])
-    print('Item IP :   '+line["managementIpAddress"])
+    print('Item ID :   ',line["id"])
+    print('Item TYPE :   ',line["type"])
+    print('Item FAMILY :   ',line["family"])
+    print('Item SOFTWARE :   ',line["softwareType"])
+    print('Item IP :   ',line["managementIpAddress"])
+    print('\n')
 
-
+#Closing Files
 file_json.close()
 
